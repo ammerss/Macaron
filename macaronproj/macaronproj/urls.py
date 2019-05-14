9b2fbeeaa46d3d8e3635fe1c6fbfee8471b9bd69
jinyoung <pjpp8588@naver.com>
2019-05-14 15:13:25 +0900

    reupdate with other modify

--- macaronproj/macaronproj/urls.py
+++ macaronproj/macaronproj/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('accounts.urls')),
     path('review/', include('review.urls', namespace = 'review')),
 ]
