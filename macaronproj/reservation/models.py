8d6c04771fc1deb156d264e729f33e81de1470bb
soobeen <b2nyb2st@naver.com>
2019-05-16 19:19:48 +0900

    db change
    model.py  is changed

--- macaronproj/reservation/models.py
+++ macaronproj/reservation/models.py
@@ -3,8 +3,12 @@ from django.db import models
 class Resevation(models.Model):
     reser_num = models.IntegerField(primary_key=True)
     customer_name = models.CharField(max_length=128)
+    shop_name = models.CharField(max_length=128)
     macaron_name = models.CharField(max_length=128)
     macaron_num = models.IntegerField(primary_key=True)
     macaron_price = models.IntegerField(primary_key=True)
     reser_date = models.DateTimeField('date published')
     reser_time = models.DateTimeField('date published')
+
+def __str__(self):
+        return self.reser_num
