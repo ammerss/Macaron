fc125470f314d90360dd49014bf2d776cd2c0525
soobeen <b2nyb2st@naver.com>
2019-05-16 19:36:58 +0900

    reservation page
    html and models.py

--- macaronproj/reservation/models.py
+++ macaronproj/reservation/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Resevation(models.Model):
+    reser_num = models.IntegerField(primary_key=True)
+    customer_name = models.CharField(max_length=128)
+    shop_name = models.CharField(max_length=128)
+    macaron_name = models.CharField(max_length=128)
+    macaron_num = models.IntegerField(primary_key=True)
+    macaron_price = models.IntegerField(primary_key=True)
+    reser_date = models.DateTimeField('date published')
+    reser_time = models.DateTimeField('date published')
+
+def __str__(self):
+        return self.reser_num
