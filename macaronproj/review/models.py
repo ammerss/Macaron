3f0a763d1da8bec846b6a79446236802217c5a93
jinyoung <pjpp8588@naver.com>
2019-05-16 18:14:44 +0900

    reupload review_page

--- macaronproj/review/models.py
+++ macaronproj/review/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Review(models.Model):
+    #store = models.ForeignKey(Store, on_delete = models.CASCADE)
+    #writer = models.CharField(max_length = 100)
+    post_date = models.DateTimeField(auto_now_add = True)
+    title = models.TextField(max_length = 50, help_text = '제목을 입력해주세요.')
+    content = models.TextField(max_length = 500, help_text = '내용을 입력해주세요.')
+    
+    def __str__(self):
+        return self.title
--- macaronproj/review/views.py
+++ macaronproj/review/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Review
+from django.utils import timezone
 
 # Create your views here.
+
+def review(request):
+    review_text = Review.objects
+    return render(request, 'review.html', {'review_text': review_text})
+
+def post_review(request):
+    review = Review()
+    review.post_data = timezone.datetime.now()
+    review.title = request.POST['head']
+    review.content = request.POST['body']
+    review.save()
+    return redirect('/review')
