bed09f650b20b4a56f9542009cefab592801db5c
weenybeenymini <alsgml9091@gmail.com>
2019-05-20 20:08:31 +0900

    delete_minhee_path

--- macaronproj/accounts/models.py
+++ macaronproj/accounts/models.py
@@ -2,4 +2,5 @@ from django.db import models
 from phonenumber_field.modelfields import PhoneNumberField
 
 class Client(models.Model):
+    user_type = models.CharField(max_length=1, null=True)
     phone = PhoneNumberField(null=False, blank=False, unique=True)
