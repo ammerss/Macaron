eff6246f0593071c49780306d920f0d32a3e8b4a
jinyoung <pjpp8588@naver.com>
2019-05-20 15:22:34 +0900

    Auto stash before merge of "dev" and "origin/dev"

--- macaronproj/reservation/models.py
+++ macaronproj/reservation/models.py
@@ -1,12 +1,14 @@
 from django.db import models
+from django.conf import settings
+from django.contrib.auth.models import User, AbstractUser
 
 class Resevation(models.Model):
     reser_num = models.IntegerField(primary_key=True)
-    customer_name = models.CharField(max_length=128)
-    shop_name = models.CharField(max_length=128)
-    macaron_name = models.CharField(max_length=128)
-    macaron_num = models.IntegerField()
-    macaron_price = models.IntegerField()
+    customer = models.ForeignKey(User, on_delete=models.CASCADE)
+    #shop_name = models.ForeignKey(Store, on_delete=models.CASCADE )
+    #choice_macaron = models.ForeignKey(Product, on_delete = models.CASCADE)
+    quantity = models.IntegerField(default = 1)
+    amount = models.IntegerField(default = 0) #결제금액
     reser_request_time = models.DateTimeField() #예약 요청 한 시각
     reser_time = models.DateTimeField(auto_now_add = True) #예약 날짜//픽업날짜
 
--- macaronproj/reservation/views.py
+++ macaronproj/reservation/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+@login_required
+def request_reservation(request):
+
+    customer = request.user
+    quantity = int(request.POST.get('quantity'))
+    shop_name = request.shop_name
+    product = Product.objects.get()
+    reser_request_time = request.time
+    reser_time = timezone.datetime.now()
+
+    if request.method == 'POST':
+        if 'reservate' in request.POST:
+            reser = Reservation()
+            reser.customer = customer
+            reser.shop_name = shop_name
+            reser.quantity = quantity
+            reser.product = product
+            reser.reser_request_time = reser_request_time
+            reser.reser_time = reser_time
