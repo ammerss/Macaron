e04c49466dc522b9dea13872c3aec6185b88b178
SiHyun Park <siso9800@naver.com>
2019-05-30 22:02:13 +0900

    logout

--- macaronproj/accounts/views.py
+++ macaronproj/accounts/views.py
@@ -31,26 +31,6 @@ def signup(request):
     else:   
         return render(request, 'signup.html')
 
-    # else:
-    #     signupform = Profile()
-    #     return render(request, 'signup.html', {'signupform':signupform})
-
-    # if request.method == 'POST':
-    #     if request.POST['password1'] == request.POST['password2']:
-    #         #아이디가 같은 경우
-    #         try:
-    #             user = User.objects.get(username=request.POST['username'])
-    #             return render(request, 'signup.html', {'error': 'Username has already been taken'})
-    #         except User.DoesNotExist:
-    #             user = User.objects.create_user(
-    #                 request.POST['username'], password=request.POST['password1'])
-    #             auth.login(request, user)
-    #             return redirect('home')
-    #     else:
-    #         return render(request, 'signup.html', {'error': 'Passwords must match'})
-    # else:
-    #     return render(request, 'signup.html')
-
 def login(request):
     if request.method == 'POST':
         username = request.POST['username']
@@ -68,7 +48,7 @@ def logout(request):
     if request.method == 'POST':
         auth.logout(request)
         return redirect('home')
-    return render(request, 'signup.html')
+    return render(request, 'login.html')
 
 @login_required
 def update_profile(request, user_id):
