04c5d73179608aab17f5f707683191f976a75641
jinyoung <pjpp8588@naver.com>
2019-06-01 22:01:33 +0900

    update reservation page

--- macaronproj/reservation/views.py
+++ macaronproj/reservation/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, reverse
+from django.utils import timezone
 from .models import Reservation
+from store.models import Store, Macarons
 from django.http import HttpResponseRedirect
+from django.contrib import messages
 
 def Reser_owner(request):
     return render(request, 'list.html')
@@ -8,29 +11,32 @@ def Reser_owner(request):
 def Reser_custom(request):
     return render(request, 'cart.html')
 
-def reserve(request):#, pk):
-    #store = get_object_or_404(Store, pk=pk)
-    return render(request, 'show.html')
+def reserve(request, pk):
+    store = get_object_or_404(Store, pk=pk)
+    product_list = store.macarons_set.all()
+    return render(request, 'show.html', {'product_list': product_list, 'store': store})
 
 
-def request_reservation(request):
+def request_reservation(request, pk):
 
-    customer = request.user
-    quantity = int(request.POST.get('quantity'))
-    shop_name = request.shop_name
-    product = Product.objects.get()
-    reser_request_time = request.time
-    reser_time = timezone.datetime.now()
+    store = get_object_or_404(Store, pk=pk)
+    #customer = request.user
+    quantity = request.POST.get('number')
+    shop_name = store
+    macaron = request.POST.get('macaron')
+    reser_request_time = timezone.datetime.now()
+    reser_time = request.POST.get('date') + request.POST.get('time')
 
     if request.method == 'POST':
-        if 'reservate' in request.POST:
-            reser = Reservation()
-            reser.customer = customer
-            reser.shop_name = shop_name
-            reser.quantity = quantity
-            reser.product = product
-            reser.reser_request_time = reser_request_time
-            reser.reser_time = reser_time
-            return HttpResponseRedirect('/reserve')
+        reser = Reservation()
+        #reser.customer = customer
+        reser.shop_name = shop_name
+        reser.quantity = quantity
+        reser.choice_macaron = macaron
+        reser.reser_request_time = reser_request_time
+        reser.reser_time = reser_time
+        reser.save()
+        messages.success(request, 'Reservation success!')
+        return HttpResponseRedirect(reverse('reservation:reserve', args=(pk,)))
 
-    return HttpResponseRedirect('/reserve')
+    return HttpResponseRedirect(reverse('reservation:reserve', args=(pk,)))
