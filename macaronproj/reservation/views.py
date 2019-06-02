6939448e80dfb14651bc9cfdc66e14dcb16df0e8
jinyoung <pjpp8588@naver.com>
2019-06-02 16:48:43 +0900

    update reserve page

--- macaronproj/reservation/views.py
+++ macaronproj/reservation/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render, get_object_or_404, reverse
+from django.contrib.auth.decorators import login_required
 from django.utils import timezone
 from .models import Reservation
 from store.models import Store, Macarons
 from django.http import HttpResponseRedirect
+from django.contrib.auth.models import User
 from django.contrib import messages
+import datetime
 
 def Reser_owner(request):
     return render(request, 'list.html')
@@ -16,20 +19,27 @@ def reserve(request, pk):
     product_list = store.macarons_set.all()
     return render(request, 'show.html', {'product_list': product_list, 'store': store})
 
-
+@login_required
 def request_reservation(request, pk):
 
-    store = get_object_or_404(Store, pk=pk)
-    #customer = request.user
-    quantity = request.POST.get('number')
-    shop_name = store
-    macaron = request.POST.get('macaron')
+    count = 1
+    customer = request.user
+    shop_name = get_object_or_404(Store, pk=pk)
+    quantity = request.POST.get('quantity')
+    macaron = ''
     reser_request_time = timezone.datetime.now()
-    reser_time = request.POST.get('date') + request.POST.get('time')
+    
+    #convert date value and time value to Datetime form
+    date = datetime.datetime.strptime(request.POST.get('date'), "%Y-%m-%d").date()
+    time = datetime.datetime.strptime(request.POST.get('time'), "%H:%M").time()
+    reser_time = datetime.datetime.combine(date, time)
 
     if request.method == 'POST':
         reser = Reservation()
-        #reser.customer = customer
+        reser.reser_num = reser_request_time.strftime('%Y%m%d') + str(shop_name.id).zfill(4) + "_" + str(count).zfill(3)
+        count += 1
+        if(count>999): count = 0
+        reser.customer = customer
         reser.shop_name = shop_name
         reser.quantity = quantity
         reser.choice_macaron = macaron
