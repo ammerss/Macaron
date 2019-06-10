a13e7dbbc48bf55f1cd7917747157daecf0bfd66
SiHyun Park <siso9800@naver.com>
2019-06-10 13:43:21 +0900

    reset

--- review/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-import review.views
-
-app_name="review"
-urlpatterns = [
-    path('review_list/<int:pk>/',review.views.review, name='review'),
-    path('review_list/<int:pk>/post_review/', review.views.post_review, name = 'post_review'),
-]
